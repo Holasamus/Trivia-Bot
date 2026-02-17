c5534bb396853cf30f7bff784bcbbe31ccca8aa3
Holasamus <tuemail@gmail.com>
2026-02-16 21:17:41 -0600

    Agrego la solucion inicial del Trivia Bot - código JS

---
+++ Trivia-bot.js
@@ -0,0 +1,27 @@
+// Variables iniciales
+let adjective = "brave";
+let noun = "dragon";
+let verb = "sneezing";
+let place = "cave";
+let adjective2 = "gigantic";
+let noun2 = "tacos";
+
+// Primera historia
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// Output primera historia
+console.log(`First story: ${firstStory}`);
+
+// Nuevos valores
+adjective = "funny";
+noun = "robot";
+verb = "dancing";
+place = "space station";
+adjective2 = "tiny";
+noun2 = "pizza";
+
+// Segunda historia
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// Output segunda historia
+console.log(`Second story: ${secondStory}`);
